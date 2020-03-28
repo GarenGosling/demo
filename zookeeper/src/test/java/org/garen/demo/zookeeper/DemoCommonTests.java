f0d6af3f42215c59e8da99b3854539bcaa6eda08
liuxueliang <Garen666>
2020-03-28 14:30:18 +0800

    普通测试

---
+++ zookeeper/src/test/java/org/garen/demo/zookeeper/DemoCommonTests.java
@@ -0,0 +1,20 @@
+package org.garen.demo.zookeeper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class DemoCommonTests {
+
+    @Test
+    void testMethod1() {
+        log.info("hello!");
+    }
+
+    @Test
+    void testMethod2() {
+        log.debug("ha ha!");
+    }
+
+
+}
