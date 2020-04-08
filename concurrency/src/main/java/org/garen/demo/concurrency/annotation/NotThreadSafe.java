1d7fe83cc4dfc9e9b2b0075871128b64f1277838
liuxueliang <Garen666>
2020-04-08 14:40:39 +0800

    concurrency init

---
+++ concurrency/src/main/java/org/garen/demo/concurrency/annotation/NotThreadSafe.java
@@ -0,0 +1,19 @@
+package org.garen.demo.concurrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * 功能描述 : 课程里用来标记【线程不安全】的类或写法
+ * </p>
+ *
+ * @author : Garen Gosling 2020/4/8 下午2:37
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+    String value() default "";
+}
---
+++ concurrency/src/main/java/org/garen/demo/concurrency/controller/HelloController.java
@@ -0,0 +1,20 @@
+package org.garen.demo.concurrency.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(description = "测试接口")
+@RestController
+@RequestMapping(value = "/")
+public class HelloController {
+
+    @ApiOperation(value="测试")
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String hello() {
+        return "test";
+    }
+
+}
