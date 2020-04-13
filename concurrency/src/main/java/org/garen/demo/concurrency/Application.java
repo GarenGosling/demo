a77dce9249cdde04385158c7962d4fdc606d9e8c
liuxueliang <Garen666>
2020-04-13 15:53:41 +0800

    线程安全策略 - 不可变对象、线程封闭

--- concurrency/src/main/java/org/garen/demo/concurrency/Application.java
+++ concurrency/src/main/java/org/garen/demo/concurrency/Application.java
@@ -1,13 +1,31 @@
 package org.garen.demo.concurrency;
 
+import org.garen.demo.concurrency.filter.HttpFilter;
+import org.garen.demo.concurrency.interceptor.HttpInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean httpFilter() {
+		FilterRegistrationBean<HttpFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new HttpFilter());
+		registrationBean.addUrlPatterns("/threadLocal/*");
+		return registrationBean;
+	}
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HttpInterceptor()).addPathPatterns("/**");
+    }
 }
